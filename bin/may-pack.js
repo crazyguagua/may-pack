04b641630b082ff19f3a35854a8d82141e1c5f67
mayufo <mayufo@aliyun.com>
2019-03-21 11:50:35 +0800

    second commit :tada:

--- bin/may-pack.js
+++ bin/may-pack.js
@@ -3,3 +3,16 @@
 // node环境
 
 console.log('start');
+
+let path = require('path')
+
+// 拿到配置文件webpack.config.js
+let config = require(path.resolve('webpack.config.js'));
+
+
+let Compiler = require('../lib/Compiler.js');
+
+let compiler = new Compiler(config);
+
+// 标识运行编译
+compiler.run()
